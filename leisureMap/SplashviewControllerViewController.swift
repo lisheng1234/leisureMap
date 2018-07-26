ec1467e2336ed390c8528dcff9f63be6961ca643
stu1 <>
2018-07-26 09:30:27 +0800

    request

--- leisureMap/SplashviewControllerViewController.swift
+++ leisureMap/SplashviewControllerViewController.swift
@@ -10,13 +10,51 @@ import UIKit
 
 class SplashviewControllerViewController: UIViewController {
 
+    @IBOutlet weak var labverson: UILabel!
     override func viewDidLoad() {
+        
+        var appversion : String = ""
+       // weak var labverson: UILabel!
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         let defaults : UserDefaults = UserDefaults.standard
         defaults.synchronize()
+        
+        appversion = "" + (Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String)!
+        labverson.text = appversion
+        //labverson.text = appversion
+        
+        let from = "https://score.azurewebsites.net/api/version/\(String(describing: appversion))"
+        let url = URL(string: from)!
+        let request = URLRequest(url: url)
+        
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration: config)
+        
+        let task = session.dataTask(with: request, completionHandler: {(data,response,error) in
+            
+            let httpResponse = response as! HTTPURLResponse
+            let statuscode = httpResponse.statusCode
+            
+            if (200 == statuscode){
+                let datastring = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+                
+                let responseStrin = String(datastring!)
+                
+                print(responseStrin)
+                
+                
+            }
+            
+            //print("here")
+        })
+        task.resume()
+        
+        
     }
+    
+    
     
 
     /*
